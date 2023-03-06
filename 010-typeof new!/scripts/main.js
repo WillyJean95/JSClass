ebdfdec9729f2000034db23558c220ecb7f949e8
Willy Jeanniton <0845477@my.scccd.edu>
2023-03-06 02:39:57 -0800

    new beginningsgit add --all

---
+++ 010-typeof new!/scripts/main.js
@@ -0,0 +1,7 @@
+let myName = "Willy"
+let myAge = "22"
+let myHappiness = true
+
+console.log ('myName is typeof ${typeof myName}')
+console.log('myAge is typeof ${typeof myAge}')
+console.log('myHappiness is typeof ${typeof myHappiness}')
