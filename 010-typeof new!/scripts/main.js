111e697bee9fb6f83425643c1b5206f718258070
Willy Jeanniton <0845477@my.scccd.edu>
2023-03-06 02:59:48 -0800

    more on variables

--- 010-typeof new!/scripts/main.js
+++ 010-typeof new!/scripts/main.js
@@ -1,7 +1,23 @@
 let myName = "Willy"
-let myAge = "22"
+let myAge = 22
 let myHappiness = true
+let newVar ='Something'
+const myConst = 22
+console.log(myAge == myName)
+console.log(myAge === myName)
+
+let myVar
+console.log('myVar is typeof ${typeof myVar}')
 
 console.log ('myName is typeof ${typeof myName}')
+console.log ('myName is value ${myName}')
 console.log('myAge is typeof ${typeof myAge}')
 console.log('myHappiness is typeof ${typeof myHappiness}')
+
+// don't use var
+if (true) {
+    const myConst = 23
+    console.log(myConst)
+} else {
+
+}
