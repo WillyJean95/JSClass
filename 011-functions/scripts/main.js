55bf236ce3ee7ce3c88436f9fa4a73696c6208ee
Willy Jeanniton <0845477@my.scccd.edu>
2023-04-28 03:04:21 -0700

    my variables

--- 011-functions/scripts/main.js
+++ 011-functions/scripts/main.js
@@ -1,12 +1,14 @@
 // Functions are fun!
 
  function doMath (a, b) { 
-    // let result = a
-    console.log('this is the result' ,(a - b))
-    console.log('this is the result' ,(a + b))
-    console.log('this is the result' ,(a / b))
-    console.log('this is the result' ,(a * b))
-    console.log('this is the result' ,(a % b))
+    let result = a * b
+    return result 
+    // console.log('this is the result' ,(a - b))
+    // console.log('this is the result' ,(a + b))
+    // console.log('this is the result' ,(a / b))
+    // console.log('this is the result' ,(a * b))
+    // console.log('this is the result' ,(a % b))
 }
 
-doMath(10, 4) 
+const result = doMath(10, 4) 
+console.log('result was returned from the function' ,(result))
