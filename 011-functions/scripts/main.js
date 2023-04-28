993f7841e27745687cece085b996c5c0180722ac
Willy Jeanniton <0845477@my.scccd.edu>
2023-04-28 03:23:17 -0700

    my arrow function

--- 011-functions/scripts/main.js
+++ 011-functions/scripts/main.js
@@ -1,19 +1,18 @@
 // Functions are fun!
 
- function doMath (a, b) { 
-    let result = a * b
-    return result 
+ doMath = (a, b) => a % b
+    console.log(doMath(50, 20))
     // console.log('this is the result' ,(a - b))
     // console.log('this is the result' ,(a + b))
     // console.log('this is the result' ,(a / b))
     // console.log('this is the result' ,(a * b))
     // console.log('this is the result' ,(a % b))
-}
+
 doMoreMath = (a, b) => a * b
     
 
 
 console.log(doMoreMath(20, 30))
 
-const result = doMath(10, 4) 
+const result = doMath(50, 20) 
 console.log('result was returned from the function' ,(result))
