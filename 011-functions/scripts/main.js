b119cdab883ee2eb6cf38c1c429b644295795e9a
Willy Jeanniton <0845477@my.scccd.edu>
2023-04-28 03:15:50 -0700

    arrow functions

--- 011-functions/scripts/main.js
+++ 011-functions/scripts/main.js
@@ -9,6 +9,11 @@
     // console.log('this is the result' ,(a * b))
     // console.log('this is the result' ,(a % b))
 }
+doMoreMath = (a, b) => a * b
+    
+
+
+console.log(doMoreMath(20, 30))
 
 const result = doMath(10, 4) 
 console.log('result was returned from the function' ,(result))
