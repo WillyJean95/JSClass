0558590ce120d59886555d06280fa76e01238ec6
Willy Jeanniton <0845477@my.scccd.edu>
2023-05-18 11:49:00 -0700

    += objects

--- 018-obj-min-globals-refractor/main.js
+++ 018-obj-min-globals-refractor/main.js
@@ -15,21 +15,10 @@ const myObj = {
     myGreeting: function(person) {
         console.log(this)
         return `${this.name} Who loves to ${this.hobbies[0]} Says Whats UP ${person}`
-    },
-    increaseLikes: function() {
-        this. likes += 1
     }
 }
-const willyObj = myObj
-willyObj. increaseLikes ()
-console.log(willyObj)
-willyObj.age = 24
 
 const greeting = myObj.myGreeting('jane')
 console.log(greeting)
 
-console.log(myObj.mobile.make.length)
-
-for (key in myObj) {
-    console.log(`${key} and value ${myObj[key]}`)
-}
+console.log(myObj.name.length)
