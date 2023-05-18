5b2c55df32590c4468ebe7758772a67afb10f8ab
Willy Jeanniton <0845477@my.scccd.edu>
2023-05-18 08:05:18 -0700

    Arrays and Looping 1

--- 013-loop-arrays/main.js
+++ 013-loop-arrays/main.js
@@ -1,4 +1,5 @@
-const myArr = []
+const myMPG = []
+const myTripCost = []
 
 const updateDOM = (input) => {
 const divEL = document.querySelector('#output')
@@ -11,15 +12,25 @@ const trackMPGCost = (miles, gallons, price = 3.79) =>  {
 const MPG = Math.round (miles/gallons)
 const tripCost = MPG * price
 updateDOM(`Mileage is ${MPG} and trip cost is ${tripCost}`)
-myArr.push(MPG, tripCost)
+myMPG.push(MPG)
+myTripCost.push(tripCost)
 }
 
-const calculateAVg = () => {
-const avgMPG = (myArr[0] + myArr[2]) / 2
-updateDOM(`Average Miles per gallon is ${avgMPG}`)
+const calculateMPGAvg = () => {
+// const avgMPG = (myArr[0] + myArr[2]) / 2
+// updateDOM(`Average Miles per gallon is ${avgMPG}`)
+let totalMPG = 0
+for(let i = 0; i < myMPG.length; i++) {
+    totalMPG = totalMPG + myMPG[i]
+}
+    let avgMPG = totalMPG/myMPG.length
+    updateDOM(`Average MPG is ${avgMPG}`)
 }
 
 trackMPGCost(300, 10, 5.40)
 trackMPGCost(320, 12, 5)
-updateDOM(myArr)
-calculateAVg()
+trackMPGCost(100, 7, 4.20)
+trackMPGCost(600, 24, 5.70)
+trackMPGCost(50, 2, 3.40)
+trackMPGCost(320, 12, 5)
+calculateMPGAvg()
