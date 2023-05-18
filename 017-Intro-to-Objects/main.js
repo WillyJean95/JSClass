6a677e79bfdb930a4325717fc11b5c7854e4cef4
Willy Jeanniton <0845477@my.scccd.edu>
2023-05-18 10:49:31 -0700

    The This

---
+++ 017-Intro-to-Objects/main.js
@@ -0,0 +1,18 @@
+const myArr = []
+
+// console.log(typeof myObj)
+// console.log(typeof myArr)
+// console.log(myArr == myObj)
+const myNumber = 23
+const myObj = { 
+    name: 'Willy',
+    age: 23,
+    hobbies: ['coding', 'running', 'workout'],
+    mobile: {
+        make: 'Iphone 11 Pro Max',
+        OS: 'IOS'
+    },
+    myGreeting: Function(person) {
+        return `What is UP ${person}`
+    }
+}
