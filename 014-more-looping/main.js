aeefcea7406a50d4fd2959760abcf3774672fcb0
Willy Jeanniton <0845477@my.scccd.edu>
2023-05-18 09:06:37 -0700

    my loops

---
+++ 014-more-looping/main.js
@@ -0,0 +1,50 @@
+const MY_MPG = []
+const MY_TRIP_COST = []
+
+const updateDOM = (input) => {
+const divEL = document.querySelector('#output')
+const h1 = document.createElement('h1')
+h1.textContent = input
+divEL.appendChild(h1)
+}
+
+const trackMPGandCost = (miles, gallons, price = 3.79) =>  {
+const MPG = Math.round(miles/gallons)
+const tripCost = Math.round(gallons * price)
+updateDOM(`Mileage is ${MPG} and trip cost is ${tripCost}`)
+MY_MPG.push(MPG)
+MY_TRIP_COST.push(tripCost)
+}
+
+const calculateSUM = (arr) => {
+    let sum = 0
+    // for(let i=0; i < arr.length; i++) {
+        // sum = sum + arr[i]
+    // }
+    // arr.forEach(element => {
+        // sum += element
+    // })
+
+    for(value of arr) {
+        sum += value
+    }
+    return sum
+}
+
+const calculateAvg = () => {
+    let sumMPG = calculateSUM(MY_MPG)
+    let sumTripCost = calculateSUM(MY_TRIP_COST)
+    let avgMPG = Math.round(sumMPG/MY_MPG.length)
+    let avgTripCost = sumTripCost/MY_TRIP_COST.length
+    updateDOM(`Average MPG is ${avgMPG}`)
+    updateDOM(`Average Trip is ${avgTripCost}`)
+}
+
+
+trackMPGandCost(300, 10, 5.40)
+trackMPGandCost(320, 12, 5)
+trackMPGandCost(100, 7, 4.20)
+trackMPGandCost(600, 24, 5.70)
+trackMPGandCost(50, 2, 3.40)
+trackMPGandCost(320, 12, 5)
+calculateAvg()
